f41f0dd0a0d310b376f9913b8070bb8fb151b9a5
Tisbus <yog78@mail.ru>
2021-12-17 17:43:32 +0300

    Start

---
+++ app/src/main/java/com/example/soccerquiz/QuizFragment.kt
@@ -0,0 +1,21 @@
+package com.example.soccerquiz
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.soccerquiz.databinding.FragmentQuizBinding
+
+
+class QuizFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val binding = DataBindingUtil.inflate<FragmentQuizBinding>(inflater, R.layout.fragment_quiz, container, false)
+        return binding.root
+    }
+}
