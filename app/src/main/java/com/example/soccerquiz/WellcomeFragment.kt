c3bf02a4e3bcd144f245031d144ec40c3f406d58
Tisbus <sim32@mail.ru>
2021-12-20 23:03:21 +0300

    make in home

--- app/src/main/java/com/example/soccerquiz/WellcomeFragment.kt
+++ app/src/main/java/com/example/soccerquiz/WellcomeFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
 import com.example.soccerquiz.databinding.ActivityMainBinding
 import com.example.soccerquiz.databinding.FragmentWellcomeBinding
 
@@ -17,6 +18,11 @@ class WellcomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val binding = DataBindingUtil.inflate<FragmentWellcomeBinding>(inflater, R.layout.fragment_wellcome, container, false)
+
+        binding.buttonLetsPlay.setOnClickListener { view : View ->
+            Navigation.findNavController(view).navigate(R.id.action_wellcomeFragment_to_quizFragment)
+        }
+
         return binding.root
     }
 }
